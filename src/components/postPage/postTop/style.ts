dcbf5412a37484bb23c256b13c527da076851c7d
chae-eunshin <115004531+chae-eunshin@users.noreply.github.com>
2024-03-04 21:56:20 +0900

    Feat: Likes and Share Buttons

--- src/components/postPage/postTop/style.ts
+++ src/components/postPage/postTop/style.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
+interface IconColorProps {
+  color: string;
+}
 const styles = {
   Container: styled.div`
     position: relative;
@@ -15,6 +18,30 @@ const styles = {
     color: #4e4e4e;
     font-size: 20px;
     padding: 30px;
+  `,
+  IconContainer: styled.div`
+    display: flex;r;
+    align-items: center;
+
+    padding:0 30px;
+  `,
+  IconImage: styled.img`
+    width: 20px;
+    height: 20px;
+
+    margin-right: 5px;
+    cursor: pointer;
+  `,
+  IconName: styled.p<IconColorProps>`
+    color: ${({ color }) => color};
+    font-size: 15px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-right: 10px;
+    cursor: pointer;
   `,
 };
 
