1399024d90f07f32f6da5d66cc57463a3cb6e002
chae-eunshin <115004531+chae-eunshin@users.noreply.github.com>
2024-03-05 22:43:48 +0900

    Style: Route

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,10 @@ function App() {
     <RecoilRoot>
       <BrowserRouter>
         <Routes>
-          <Route path="/boardPage/" element={<BoardPage />} />{' '}
-          {/* 인덱스 라우트를 정의 이는 /boardPage 경로에 접속했을 때 기본적으로 보여줄 컴포넌트를 지정 */}
-          <Route index element={<BoardPage />} />
-          <Route path="/boardPage/:kind/" element={<BoardPage />} />
-          <Route path="/boardPage/:kind/:choose" element={<BoardPage />} />
+          <Route path="/boardPage/" element={<BoardPage />}>
+            <Route path=":kind/" element={<BoardPage />} />
+            <Route path=":kind/:choose" element={<BoardPage />} />
+          </Route>
           <Route path="/PostPage" element={<PostPage />} />
         </Routes>
       </BrowserRouter>
